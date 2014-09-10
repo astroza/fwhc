96ec4cd61ba698b653c197787325f14700c16796
Felipe Astroza <felipe@astroza.cl>
2014-09-10 16:14:15 -0300

    first commit

---
+++ fwhc.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name       FWHC
+// @namespace  http://astroza.cl
+// @version    0.1
+// @description Script to hide annoying comments from usual FW users
+// @match      http://www.fayerwayer.com/*
+// @copyright  2012+, Felipe Astroza
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+$("<h2>Los comentarios fueron omitidos por FWHC</h2>").insertAfter('#disqus_thread');
+$('#disqus_thread').hide("");
+
